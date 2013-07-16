b33b8c5911bd10e0352f2bcb9ac5b18658cff3b8
hockeydraft <hockeydraft@spurs.dreamhost.com>
2013-07-15 19:16:53 -0700

    Adding default route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Draft::Application.routes.draw do
+  root :to => 'drafts#index'
+
   resources :players
 
 
@@ -57,7 +59,6 @@ Draft::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
